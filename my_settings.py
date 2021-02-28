00ba232ac80ab09734c1ceaf1d70c48ac9ff0ac2
sonhl0723 <sonhl0723@naver.com>
2021-02-28 18:04:37 +0900

    add db user

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+# import pymysql
+# pymysql.install_as_MySQLdb()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',   # engine
+        'NAME': 'django_study', # db
+        'USER': 'tft',         # user
+        'PASSWORD': '1234',  # user password
+        'HOST': 'localhost',    # host
+        'PORT': '3306',         # port
+    }
+}
+SECRET_KEY = 'z-5z9bw_j_*0#u4qjez7fd9#t9pc0%$^xko)e1a*!k*u66vu=-'
