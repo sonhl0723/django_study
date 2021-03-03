0642b2d1776b5ada360730d847a6125ac4481789
sonhl0723 <sonhl0723@naver.com>
2021-03-04 00:45:12 +0900

    _

--- tft/views.py
+++ tft/views.py
@@ -25,8 +25,8 @@ def search(request):
     return render(request, 'search.html', {'user_info':user_info})
 
 def search_result(request):
-    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+    # GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     # 새로운 chrome 창 안뜨게 설정
     driver_options = webdriver.ChromeOptions()
     
@@ -34,7 +34,7 @@ def search_result(request):
     driver_options.add_argument("--disable-dev-shm-usage")
     driver_options.add_argument("--no-sandbox")
     driver_options.add_argument('lang=ko_KR') # 언어 설정
-    driver_options.binary_location = GOOGLE_CHROME_PATH
+    driver_options.binary_location = GOOGLE_CHROME_BIN
 
     # riot developer 접속
     # driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=driver_options) 
