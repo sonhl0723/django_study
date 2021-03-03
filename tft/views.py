f5b5cd6c39f9326d431d7df6db10b372d5b23bd0
sonhl0723 <sonhl0723@naver.com>
2021-03-04 01:30:47 +0900

    heroku에서 사용가능

--- tft/views.py
+++ tft/views.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 # riot api 키 복사 후 붙여넣기 기능 모듈
 import pyperclip
 import os
+from webdriver_manager.chrome import ChromeDriverManager
 
 def login(request):
     return render(request, 'login.html')
@@ -25,8 +26,8 @@ def search(request):
     return render(request, 'search.html', {'user_info':user_info})
 
 def search_result(request):
-    # GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-    # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+    # CHROMEDRIVER_PATH = '/hongdjtest/.chromedriver/bin/chromedriver'
+    # GOOGLE_CHROME_PATH = '/hongdjtest/.apt/usr/bin/google_chrome'
     # 새로운 chrome 창 안뜨게 설정
     driver_options = webdriver.ChromeOptions()
     
@@ -35,10 +36,12 @@ def search_result(request):
     driver_options.add_argument("--no-sandbox")
     driver_options.add_argument('lang=ko_KR') # 언어 설정
     # driver_options.binary_location = GOOGLE_CHROME_PATH
+    # driver_options.binary_location = os.environ.get('GOOGLE_CHROME_PATH')
 
     # riot developer 접속
-    driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=driver_options) 
+    # driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=driver_options) 
     # driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=driver_options)
+    driver = webdriver.Chrome(chrome_options=driver_options, executable_path=ChromeDriverManager().install())
     driver.get("https://developer.riotgames.com/")
 
     # LOGIN 버튼 클릭
