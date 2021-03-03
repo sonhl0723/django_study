8c55c4bc4ff1493de5d870b311354a2dfb48ede2
sonhl0723 <sonhl0723@naver.com>
2021-03-04 02:33:46 +0900

    delete

--- djtest/settings.py
+++ djtest/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'z-5z9bw_j_*0#u4qjez7fd9#t9pc0%$^xko)e1a*!k*u66vu=-'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
